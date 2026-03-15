742dfcecf3a2b90434d59ed897285c9cd17fe36a
ajatusvirhe <164178363+ajatusvirhe@users.noreply.github.com>
2026-03-15 13:56:09 +0200

    football game toimii

--- Assets/scripts/TeleportSpot.cs
+++ Assets/scripts/TeleportSpot.cs
@@ -4,8 +4,8 @@ public class TeleportSpot : Interactive
 {
     public new void Interact()
     {
-        //RaycastHit hit = CameraInteract.GetLatestHit();
-        CameraInteract.GetPlayerTransform().position =
-            transform.position * CameraInteract.GetPlayerHeight();
+        var player = CameraInteract.GetPlayerTransform();
+        // place player at the spot position, offset by player height along the spot's up direction
+        player.position = transform.position + transform.up * CameraInteract.GetPlayerHeight();
     }
 }

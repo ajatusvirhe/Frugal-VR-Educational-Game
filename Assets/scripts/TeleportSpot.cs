7e98c4ebc670b9a5774e6fc083c2c4c1fbdf5b96
ajatusvirhe <164178363+ajatusvirhe@users.noreply.github.com>
2026-03-18 23:54:37 +0200

    teleportspot

--- Assets/scripts/TeleportSpot.cs
+++ Assets/scripts/TeleportSpot.cs
@@ -1,11 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class TeleportSpot : Interactive
 {
+    private float _teleportCooldown = 0.5f;
+    private bool _isOnCooldown;
     public new void Interact()
     {
         var player = CameraInteract.GetPlayerTransform();
         // place player at the spot position, offset by player height along the spot's up direction
         player.position = transform.position + transform.up * CameraInteract.GetPlayerHeight();
+
+        StartCoroutine(TeleportCooldownRoutine());
+    }
+
+    private IEnumerator TeleportCooldownRoutine()
+    {
+        _isOnCooldown = true;
+        yield return new WaitForSeconds(_teleportCooldown);
+        _isOnCooldown = false;
     }
 }
